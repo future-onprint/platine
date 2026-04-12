592cb746d2b95f9a4a3fb9f89854d84037835eed
Kylian-Mallet <kylian-mallet@hotmail.fr>
2026-04-12 14:45:08 +0100

    fix: generate Platine Log IDs with full timestamp via autoname() controller

--- platine/platine/doctype/platine_log/platine_log.py
+++ platine/platine/doctype/platine_log/platine_log.py
@@ -1,6 +1,11 @@
 import frappe
 from frappe.model.document import Document
+from frappe.model.naming import make_autoname
+from frappe.utils import now_datetime
 
 
 class PlatineLog(Document):
-    pass
+    def autoname(self):
+        dt = now_datetime()
+        prefix = dt.strftime("LOG-%d-%m-%Y-%H-%M-%S-")
+        self.name = make_autoname(prefix + ".##")
